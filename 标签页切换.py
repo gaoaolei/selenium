e02a330eb47b02d11e9f47f584b5a40391c99700
DESKTOP-RG0DRJU\admin <853573584@qq.com>
2021-11-08 11:19:31 +0800

    改名字

--- 标签页切换.py
+++ 标签页切换.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 
 driver = webdriver.Chrome()
 driver.get("http://www.baidu.com")
@@ -7,6 +8,7 @@ current_window = driver.current_window_handle
 print(current_window)
 all_Windows = driver.window_handles
 print(all_Windows)
+
 # 切到第二个窗口
 # driver.switch_to.window(all_Windows[1])
 # 数index很不准，所以要具体定位，如下：
@@ -16,4 +18,8 @@ for handle in all_Windows:
         break
 a = driver.current_window_handle
 print(a)
-print(driver.current_url)   # 打印当前标签页的url   asdf
+print(driver.current_url)   # 打印当前标签页的url
+print(driver.title)
+driver.close()
+time.sleep(1)
+driver.quit()
